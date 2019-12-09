ae0933030c72dbc3c87db8cc8594b2211dfdcef5
ItsErikSquared <erik@itserik.dev>
2019-12-08 19:34:07 -0500

    🖕

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 
-// Copyright ItsErikSquared 2019
-
 console.log('%cHey!', 'font-size:100px; color:red')
 console.log('%cWhy are you looking at my code?', 'font-size:20px; color:white')
 console.warn('%cIt\'s a horrible example! Try stackoverflow, or a better developer\'s site.', 'font-size:20px; color:white')
-console.error('%cIt\'s also copyrighted...', 'font-size: 20px; color:white')
+console.error('%cGo ahead and steal it because ya know who gives a fuck about it anyways...', 'font-size: 20px; color:white')
 console.log('%cI know, I know... you wanted to look... here\'s a cookie for your troubles...', 'font-size:20px; color:white')
 // https://stackoverflow.com/a/57238009
 console.image = function (url, size = 100) {
